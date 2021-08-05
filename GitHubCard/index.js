ff45f44f4ef4431d7b8b275eca24ac8296cc3196
Sebastian Matthew Leal <sebastianmatthewleal@Sebastians-MacBook-Pro.local>
2021-08-05 16:26:58 -0700

    almost there

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,21 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+import axios from 'axios';
+// /*
+  // STEP 1: using axios, send a GET request to the following URL
+  //   (replacing the placeholder with your Github name):
+  //   https://api.github.com/users/<your name>
 
+    
+
+axios.get('https://api.github.com/users/sebastianleal')
+  .then(res => {
+    console.log(res)
+    const seb = cardCreator(res.data)
+    document.querySelector('.cards').appendChild(seb)
+    console.log(seb )
+  })
+  .catch(err => {
+    console.error(err)
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,7 +61,55 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardCreator({avatar_url, name, login, location, html_url, followers, following, bio}) {
+  //create
+  const card = document.createElement('div')
+  const image = document.createElement('img')
+  const info = document.createElement('div')
+  const myName = document.createElement('h3')
+  const userName = document.createElement('p')
+  const place = document.createElement('p')
+  const profile = document.createElement('p')
+  const link = document.createElement('a')
+  const myFollowers = document.createElement('p')
+  const myFollowing = document.createElement('p')
+  const myBio = document.createElement('p')
 
+  //append child 
+  card.appendChild(image)
+  card.appendChild(info)
+  info.appendChild(myName)
+  info.appendChild(userName)
+  info.appendChild(place)
+  info.appendChild(profile)
+  profile.appendChild(link)
+  info.appendChild(myFollowers)
+  info.appendChild(myFollowing)
+  info.appendChild(myBio)
+  
+  //add class 
+  card.classList.add('card')
+  image.setAttribute('src', `{avatar_url}`)
+  image.setAttribute('alt',`This is a picture of ${name}`)
+  info.classList.add('card-info')
+  myName.classList.add('name')
+  myName.textContent = `Name: ${name}`
+  userName.classList.add('username')
+  userName.textContent = `Username: ${login}`
+  place.textContent = `Location: ${location ? location : 'I have no location'}`
+  profile.textContent = 'Profile:'
+  link.setAttribute('src', `${html_url}`)
+  link.textContent = html_url
+  myFollowers.textContent = `Follower: ${followers}`
+  myFollowing.textContent = `Following: ${following}`
+  myBio.textContent = `Bio: ${bio ? bio: 'I have no bio'}`
+
+  console.log(profile)
+  console.log(link)
+
+  return card 
+  
+}
 /*
   List of LS Instructors Github username's:
     tetondan
